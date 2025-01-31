ca96b084b0e8dbe5e318c7e315390d9211b110dc
NoahMil <noah.miliani13@gmail.com>
2025-01-31 13:01:04 +0100

    Modified New Player Controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,6 +19,23 @@ namespace Player
         private RaycastHit2D _headHit;
         private bool _isGrounded;
         private bool _bumpedHead;
+        
+        public float VerticalVelocity { get; private set; }
+        private bool _isJumping;
+        private bool _isFastFalling;
+        private bool _isFalling;
+        private float _fastFallTime;
+        private float _fastFallReleaseSpeed;
+        private int _numberOfJumpsUsed;
+
+        private float _apexPoint;
+        private float _timePastApexThreshold;
+        private bool _isPastApexThreshold;
+
+        private float _jumpBufferTimer;
+        private bool _jumpReleasedDuringBuffer;
+        
+        private float _coyoteTimer;
 
         private void Awake()
         {
@@ -39,6 +56,12 @@ namespace Player
             {
                 Move(MovementStats.AirAcceleration, MovementStats.AirDeceleration, InputManager.Movement);
             }
+        }
+
+        private void Update()
+        {
+            CountTimers();
+            Jump();
         }
 
         #region Movement
@@ -92,6 +115,156 @@ namespace Player
             {
                 _isFacingRight = false;
                 transform.Rotate(0f, -180f, 0f);
+            }
+        }
+
+        #endregion
+
+        #region Jump
+
+        private void JumpChecks()
+        {
+            if (InputManager.JumpWasPressed)
+            {
+                _jumpBufferTimer = MovementStats.JumpBufferTime;
+                _jumpReleasedDuringBuffer = false;
+            }
+
+            if (InputManager.JumpWasReleased)
+            {
+                if (_jumpBufferTimer > 0f)
+                {
+                    _jumpReleasedDuringBuffer = true;
+                }
+
+                if (_isJumping && VerticalVelocity > 0f)
+                {
+                    if (_isPastApexThreshold)
+                    {
+                        _isPastApexThreshold = false;
+                        _isFastFalling = true;
+                        _fastFallTime = MovementStats.TimeForUpwardsCancel;
+                        VerticalVelocity = 0f;
+                    }
+                    else
+                    {
+                        _isFastFalling = true;
+                        _fastFallReleaseSpeed = VerticalVelocity;
+                    }
+                }
+            }
+
+            if (_jumpBufferTimer > 0f && !_isJumping && (_isGrounded || _coyoteTimer > 0f))
+            {
+                InitiateJump(1);
+
+                if (_jumpReleasedDuringBuffer)
+                {
+                    _isFastFalling = true;
+                    _fastFallReleaseSpeed = VerticalVelocity;
+                }
+            }
+
+            else if (_jumpBufferTimer > 0f && _isJumping && _numberOfJumpsUsed < MovementStats.NumberOfJumpsAllowed)
+            {
+                _isFastFalling = false;
+                InitiateJump(1);
+            }
+
+            else if (_jumpBufferTimer > 0f && _isFalling && _numberOfJumpsUsed < MovementStats.NumberOfJumpsAllowed)
+            {
+                InitiateJump(2);
+                _isFastFalling = false;
+            }
+
+            if ((_isJumping || _isFalling) && _isGrounded && VerticalVelocity <= 0f)
+            {
+                _isJumping = false;
+                _isFalling = false;
+                _isFastFalling = false;
+                _fastFallTime = 0f;
+                _isPastApexThreshold = false;
+                _numberOfJumpsUsed = 0;
+                
+                VerticalVelocity = Physics2D.gravity.y;
+            }
+        }
+
+        private void Jump()
+        {
+            if (_isJumping)
+            {
+                if (_bumpedHead)
+                {
+                    _isFastFalling = true;
+                }
+            }
+
+            if (VerticalVelocity >= 0f)
+            {
+                _apexPoint = Mathf.InverseLerp(MovementStats.InitialJumpVelocity, 0f, VerticalVelocity);
+
+                if (_apexPoint > MovementStats.ApexThreshold)
+                {
+                    if (!_isPastApexThreshold)
+                    {
+                        _isPastApexThreshold = true;
+                        _timePastApexThreshold = 0f;
+                    }
+
+                    if (_isPastApexThreshold)
+                    {
+                        _timePastApexThreshold += Time.fixedDeltaTime;
+                        if (_timePastApexThreshold < MovementStats.ApexHangTime)
+                        {
+                            VerticalVelocity = 0f;
+                        }
+                        else
+                        {
+                            VerticalVelocity = -0.01f;
+                        }
+                    }
+
+                    else
+                    {
+                        VerticalVelocity += MovementStats.Gravity * Time.fixedDeltaTime;
+                        if (_isPastApexThreshold)
+                        {
+                            ;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void InitiateJump(int numberOfJumpsUsed)
+        {
+            if (!_isJumping)
+            {
+                _isJumping = true;
+            }
+
+            _jumpBufferTimer = 0f;
+            _numberOfJumpsUsed += numberOfJumpsUsed;
+            VerticalVelocity = MovementStats.InitialJumpVelocity;
+        }
+        
+
+        #endregion
+
+        #region Timers
+
+        private void CountTimers()
+        {
+            _jumpBufferTimer -= Time.deltaTime;
+
+            if (!_isGrounded)
+            {
+                _coyoteTimer -= Time.deltaTime;
+            }
+            else
+            {
+                _coyoteTimer -= MovementStats.JumpCoyoteTime;
             }
         }
 
